15b7b8c6aa3acce320ce0b0d54dff5ca47270f45
Angela Olovson <angela.olovson@gmail.com>
2025-06-01 17:22:31 -0400

    feat(Work): Style

--- src/data/jobs.js
+++ src/data/jobs.js
@@ -2,7 +2,8 @@ export default [
     {
         jobTitle:"Global Technology Volunteer - Frontend Developer", 
         company:"Bridges for Enterprise, Remote", 
-        date:"Dec 2023 - Present", 
+        date:"Dec 2023 - Present",
+        jobDescriptions:"Collaborate with the internal and design teams on the organization's public website’s redevelopment using Astro and Tailwind.",
         link:"https://www.bridgesforenterprise.com/",
         linkText:"Bridges for Enterprise",
     },
--- src/types/job.ts
+++ src/types/job.ts
@@ -2,6 +2,7 @@ export type Job = {
     jobTitle: string;
     company: string;
     date: string;
+    jobDescriptions:string[];
     link?: string;
     linkText?: string;
 }
