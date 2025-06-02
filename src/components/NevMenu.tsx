cbb7e08049bb43759c11b34b65f953a8aac31b5b
Angela Olovson <angela.olovson@gmail.com>
2025-06-01 21:23:11 -0400

    feat(NevMenu): Install React and create component for nev bar

---
+++ src/components/NevMenu.tsx
@@ -0,0 +1,12 @@
+export default function NevMenu() {
+    return (
+        <nav className="flex gap-10">
+            <div>logo</div>
+            <div>
+                <a href="#about">About</a>
+                <a href="#resume">Resume</a>
+                <a href="#projects">Projects</a>
+            </div>
+        </nav>
+    );
+}
