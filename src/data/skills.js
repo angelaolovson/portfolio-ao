2e5e4c43137bce6209d74ebc5b3bf28e4570ae4e
Angela Olovson <angela.olovson@gmail.com>
2025-06-05 14:18:42 -0400

    feat(Skills): Create componenet, data, type

---
+++ src/data/skills.js
@@ -0,0 +1,12 @@
+export default [
+    { skillTitle: "Programming Languages", skillInfo: "JavaScript / TypeScript · HTML5 · CSS3 · Python · EJS" },
+    { skillTitle: "Frameworks", skillInfo: "React · Express.js · Astro · Django" },
+    { skillTitle: "Libraries", skillInfo: "Bootstrap · Tailwind CSS · jQuery" },
+    { skillTitle: "Databases", skillInfo: "MongoDB · PostgreSQL" },
+    { skillTitle: "Tools & Other Technologies", skillInfo: "Git · VS Code · Postman · RESTful APIs · JSON" },
+    { skillTitle: "Microsoft Office", skillInfo: "Excel · Word · PowerPoint · Outlook" },
+    { skillTitle: "Adobe Creative Suite", skillInfo: "Photoshop · Illustrator · InDesign" },
+    { skillTitle: "Attention to Detail", skillInfo: "Consistently ensure precision and high-quality output in both design and code." },
+    { skillTitle: "Communication", skillInfo: "Effectively interpret and communicate challenges to teammates, fostering collaborative problem-solving." },
+  ];
+  
